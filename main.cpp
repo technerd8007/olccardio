9193a5c07d0d72600eabe135e87e463d2eecf4ab
Nicholas Waugh <deathwillcome800@gmail.com>
2020-06-16 16:10:11 -0400

    Main.cpp
    Added FMOD support for sound support with a high level of audio manipulation capability.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #define OLC_PGE_APPLICATION
 #include "olcPixelGameEngine.h"
+#include "fmod.hpp"
 #include <string>
 
 using namespace olc;
@@ -10,28 +11,48 @@ Decal *Menu = nullptr;
 std::string station;
 std::string song;
 
+//Selection Of Menu
+int selection;
+
+FMOD::System *sis;
+FMOD::Channel *channel = 0;
+FMOD::Sound *sound1;
+
+
 // Override base class with your custom functionality
 class Example : public olc::PixelGameEngine
 {
+
+
 public:
 	Example()
 	{
 		// Name you application
 		sAppName = "Example";
+
 	}
 
 public:
+
 	bool OnUserCreate() override
 	{
+		FMOD::System_Create(&sis);      // Create the main system object.
+
+		sis->init(32, FMOD_INIT_NORMAL, NULL);    // Initialize FMOD.
+		sis->createSound("./music/sample.mp3", FMOD_CREATESTREAM, 0, &sound1);
+
 		// Called once at the start, so create things here
 		menu = new Sprite("mainMenu.png");
 		Menu = new Decal(menu);
+
+		selection = 0;
 
 		return true;
 	}
 
 	bool OnUserUpdate(float fElapsedTime) override
 	{
+
 		//Clear Frame
 		Clear(WHITE);
 
@@ -44,18 +65,57 @@ public:
 		{
 			return 0;
 		}
-
-		DrawStringDecal({ 20, 50 }, "Radio Station: ", WHITE, { 1.45f, 1.45f });
-		DrawStringDecal({ 20, 80 }, "Song: ", WHITE, { 1.45f, 1.45f });
-		if (station == "")
+		if (GetKey(olc::Key::K1).bPressed)
 		{
-			DrawStringDecal({ 190, 50 }, "! UNKNOWN !", YELLOW, { 1.45f, 1.45f });
+			sis->playSound(sound1, 0, false, &channel);
+			selection = 0;
+		}
+		if (GetKey(olc::Key::K2).bPressed)
+		{
+			selection = 1;
+		}
+		if (GetKey(olc::Key::K3).bPressed)
+		{
+			selection = 2;
+		}
+		if (GetKey(olc::Key::K4).bPressed)
+		{
+			selection = 3;
 		}
 
-		if (song == "")
+		switch (selection)
 		{
-			DrawStringDecal({ 85, 80 }, "! UNKNOWN !", YELLOW, { 1.45f, 1.45f });
+		case 0:
+			DrawStringDecal({ 20, 50 }, "Radio Station: ", WHITE, { 1.45f, 1.45f });
+			DrawStringDecal({ 20, 80 }, "Song: ", WHITE, { 1.45f, 1.45f });
+			DrawStringDecal({ 20, 110 }, "Word Of The Day", WHITE, { 1.45f, 1.45f });
+			if (station == "")
+			{
+				DrawStringDecal({ 190, 50 }, "! UNKNOWN !", YELLOW, { 1.45f, 1.45f });
+			}
+
+			if (song == "")
+			{
+				DrawStringDecal({ 85, 80 }, "! UNKNOWN !", YELLOW, { 1.45f, 1.45f });
+			}
+
+
+			break;
+		case 1:
+			DrawStringDecal({ 20, 50 }, "Downloaded Music", WHITE, { 1.45f, 1.45f });
+
+
+
+
+			break;
+		case 2:
+
+			break;
+		case 3:
+
+			break;
 		}
+
 		return true;
 	}
 };
